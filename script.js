37b707012079adb96ed48f4f60ad0b65cf625056
bhamu1432-collab <bhamu1432@gmail.com>
2026-03-19 19:54:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
-console.log("Bhagwan Decoration Website Loaded");
+function toggleSidebar() {
+  sidebar.classList.toggle("active");
+  overlay.classList.toggle("active");
+}
+
+function closeSidebar() {
+  sidebar.classList.remove("active");
+  overlay.classList.remove("active");
+}
+
+function openMap() {
+  window.location.href = "https://maps.app.goo.gl/zHJByjEHvwHVsNNk9";
+}
+
+function openCategory(cat) {
+  window.location.href = "products.html?cat=" + cat;
+}
+
+function goHome() {
+  window.location.href = "index.html";
+}
+
+/* Search */
+function searchItems() {
+  let input = document.getElementById("searchInput").value.toLowerCase();
+  let cards = document.getElementsByClassName("app-card");
+  let found = false;
+
+  for (let i = 0; i < cards.length; i++) {
+    let text = cards[i].innerText.toLowerCase();
+
+    if (text.includes(input)) {
+      cards[i].style.display = "block";
+      found = true;
+    } else {
+      cards[i].style.display = "none";
+    }
+  }
+
+  document.getElementById("noResult").style.display = found ? "none" : "block";
+}
